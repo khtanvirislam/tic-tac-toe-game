2296a8074aeec36942e5942ffa34ff6f4ed55de8
Khtanvirislam <khtanvir2020@gmail.e.com>
2024-01-11 12:30:35 +0600

    basic project is ready with js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+let boxes = document.querySelectorAll(".box");
+let resetButton = document.getElementById("reset-btn");
 
+let turnO = true;
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 4, 6],
+  [2, 5, 8],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = " X";
+      turnO = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
+
+const checkWinner = () => {
+  for (let winPattern of winPatterns) {
+    console.log(winPattern[0], winPattern[1], winPattern[2]);
+  }
+};
